8bdd5e4e5ea9f22cb8af618fb3f749965f1614b8
bramaprasetyopermana <bramprasetyop@gmail.com>
2018-06-07 19:39:27 +0700

    book-delete-put-getById

--- controllers/books.js
+++ controllers/books.js
@@ -1,21 +1,21 @@
-const Book = require('./../models/book');
+const Book = require("./../models/book");
 
-exports.addBook = (req, res) => {
-}
+exports.addBook = (req, res) => {};
 
-exports.getInfo = (req, res) => {
+exports.getInfo = (req, res) => {};
 
-}
-
-exports.getBooks = (req, res) => {
-
-}
-
+exports.getBooks = (req, res) => {};
+//========= put ======
 exports.updateBook = (req, res) => {
+  Book.findByIdAndUpdate()
+    .then(book => {
+      res.status(200).json({ message: "update book success" });
+    })
+    .catch(err => {
+      res.status(400).json({ message: "update failed" });
+    });
+};
+//========= delete ======
+exports.deleteBook = (req, res) => {};
 
-}
-
-exports.deleteBook = (req, res) => {
-  
-}
-
+exports.getBookById = (req, res) => {};
