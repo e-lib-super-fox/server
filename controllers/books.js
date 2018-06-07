7104a3f3c8fd1394f615f7d0af681b7e0f11d7f5
Dwiky Erlangga <dwikyerl@gmail.com>
2018-06-07 19:08:25 +0700

    Define routes for books

---
+++ controllers/books.js
@@ -0,0 +1,21 @@
+const Book = require('./../models/book');
+
+exports.addBook = (req, res) => {
+}
+
+exports.getInfo = (req, res) => {
+
+}
+
+exports.getBooks = (req, res) => {
+
+}
+
+exports.updateBook = (req, res) => {
+
+}
+
+exports.deleteBook = (req, res) => {
+  
+}
+
