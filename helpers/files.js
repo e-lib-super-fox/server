026c636ddd523bcf3df94b04ec86522ffb266119
Dwiky Erlangga <dwikyerl@gmail.com>
2018-06-07 20:17:08 +0700

    Fix conflict in files helpers

--- helpers/files.js
+++ helpers/files.js
@@ -11,17 +11,12 @@ const storage = Storage({
 exports.multer = Multer({
   storage: Multer.memoryStorage(),
   limits: {
-<<<<<<< HEAD
     fileSize: 50 * 1024 * 1024,
-=======
-    fileSize: 5 * 1024 * 1024,
->>>>>>> f0185eb795f1b0f1a1c70d3f8421b2d7c2a5951f
   },
 });
 
 const bucket = storage.bucket(CLOUD_BUCKET);
 
-<<<<<<< HEAD
 exports.sendUploadToGCS = (req, res, next) => {
   if (!req.file) {
     return next();
@@ -53,45 +48,4 @@ exports.sendUploadToGCS = (req, res, next) => {
   });
 
   stream.end(req.file.buffer);
-=======
-const uploadSingleFile = function(inputFile) {
-  return new Promise(function(resolve, reject) {
-    const gcsname = Date.now() + inputFile.originalname;
-
-    const publicUrl = `https://storage.googleapis.com/${CLOUD_BUCKET}/${gcsname}`;
-
-    const file = bucket.file(gcsname);
-
-    const stream = file.createWriteStream({
-      metadata: {
-        contentType: inputFile.mimetype,
-      },
-    });
-
-    stream.on('error', err => {
-      inputFile.cloudStorageError = err;
-      reject(err);
-    });
-
-    stream.on('finish', () => {
-      inputFile.cloudStorageObject = gcsname;
-      file.makePublic().then(() => {
-        inputFile.cloudStoragePublicUrl = publicUrl;
-        resolve();
-      });
-    });
-
-    stream.end(inputFile.buffer);
-  });
-};
-
-exports.sendUploadToGCS = (req, res, next) => {
-  if (!req.files) {
-    return next();
-  }
-
-  Promise.all(req.files.map(file => uploadSingleFile(file))).then(values => {
-    next();
-  });
->>>>>>> f0185eb795f1b0f1a1c70d3f8421b2d7c2a5951f
 };
