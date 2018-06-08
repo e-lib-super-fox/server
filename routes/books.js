ed1b47846629d7b1650adc767e77e4364afe83b1
Dwiky Erlangga <dwikyerl@gmail.com>
2018-06-08 09:29:05 +0700

    Update routes for books

--- routes/books.js
+++ routes/books.js
@@ -4,18 +4,18 @@ const booksControllers = require('./../controllers/books');
 const { multer, sendUploadToGCS  } = require('./../helpers/files');
 var { auth } = require("../helpers/customerMiddlewares");
 
-router.route('/')
-  .post(
-    auth,
-    multer.single('file'),
-    sendUploadToGCS,
-    booksControllers.addBook) // Add a book
-  .get(auth, booksControllers.getBooks) // Get all books
+router.post('/add',
+  auth,
+  multer.fields([{name:'file'}, {name: 'image'}]),
+  sendUploadToGCS,
+  booksControllers.addBook) // Add a book
+  
+router.get('/', booksControllers.getBooks) // Get all books
 
 router.get('/info', auth, booksControllers.getInfo);
 
 router.route('/:bookId')
-  .get(auth, booksControllers.getById) // Get book by Id
+  .get(booksControllers.getById) // Get book by Id
   .put(auth, booksControllers.updateBook) // Update book by Id
   .delete(auth, booksControllers.deleteBook) // Delete book by Id
 
