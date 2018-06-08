ffee945fdf1015506c210d4cc62ad14144522ef1
bramaprasetyopermana <bramprasetyop@gmail.com>
2018-06-08 10:50:05 +0700

    add-validation-signup-login

--- helpers/signupValidation.js
+++ helpers/signupValidation.js
@@ -2,9 +2,17 @@
 const { check, validationResult } = require("express-validator/check");
 // ============= tambahan npm install --save express-validator =========
 var signUpVal = [
+  check("userName")
+    .trim()
+    .isLength({ min: 6 })
+    .matches(/^\w+$/)
+    .withMessage("invalid username"),
+  
   check("email")
+    .isLength({ min: 1 })
+    .trim()
     .isEmail()
-    .withMessage("must be an email"),
+    .withMessage("invalid email"),
   check("password")
     .isLength({ min: 6 })
     .withMessage("passwords must be at least 6 chars long")
